76fce845de510b850f4dddb126813dcdd182c557
Jayesh KR <kr.jayesh94@gmail.com>
2015-12-06 22:52:41 +0530

    firstcommit
    first time on git

---
+++ app/src/main/java/com/example/pingme/loginphp/Register.java
@@ -0,0 +1,37 @@
+package com.example.pingme.loginphp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by PingMe on 12/5/2015.
+ */
+public class Register extends Activity {
+
+    EditText ET_NAME,ET_USER_NAME,ET_USER_PASS;
+    String name,user_name,user_pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+
+
+        ET_NAME=(EditText)findViewById(R.id.name);
+        ET_USER_NAME=(EditText)findViewById(R.id.new_user_name);
+        ET_USER_PASS=(EditText)findViewById(R.id.new_user_pass);
+    }
+
+    public void userReg(View view)
+    {
+        name=ET_NAME.getText().toString();
+        user_name=ET_USER_NAME.getText().toString();
+        user_pass=ET_USER_PASS.getText().toString();
+        String method="register";
+        BackgroundTask backgroundTask=new BackgroundTask(this);
+        backgroundTask.execute(method,name,user_name,user_pass);
+        finish();
+    }
+
+}
